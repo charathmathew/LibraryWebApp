72be5a3d5ba946a563e2d2abad8f9ec3b0fd7d71
charathmathew <charathm@mail.gvsu.edu>
2019-10-27 22:21:11 -0400

    initial commit

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -4,13 +4,7 @@ import "firebase/database";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyB1PW0LRW0GaDRGsnecl4jTDg8kUqn9GzI",
-    authDomain: "webapp-743e1.firebaseapp.com",
-    databaseURL: "https://webapp-743e1.firebaseio.com",
-    projectId: "webapp-743e1",
-    storageBucket: "webapp-743e1.appspot.com",
-    messagingSenderId: "606287587594",
-    appId: "1:606287587594:web:1be8d4398fdb82f045f2f3"
+    //put firebase sdk here
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
